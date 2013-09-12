ad0475f49b415af0c7e6914cad8febd7d9967483
Michael Hayes <tasslehawf@mac.com>
2013-09-12 15:52:08 -0500

    script adds ENV['PATH'] = "/opt/rumm/embedded/bin:#{ENV['PATH']}" to bin/rumm

--- config/software/rumm.rb
+++ config/software/rumm.rb
@@ -2,6 +2,7 @@ name "rumm"
 version "1.0.0"
 
 dependency "libxml2"
+dependency "libxslt"
 dependency "nokogiri"
 dependency "rubygems"
 dependency "bundler"
@@ -10,4 +11,15 @@ dependency "bundler"
 
 build do
   gem "install rumm"
+
+  FileUtils.mkdir_p "#{install_dir}/bin/"
+  Dir["#{Omnibus::Config.project_root}/scripts/*"].each do |s|
+    sname = File.basename(s)
+    dest = "#{install_dir}/bin/#{sname}"
+    File.open(dest, 'w') do |f|
+      f.puts "#!#{install_dir}/embedded/bin/ruby"
+      f.puts File.read(s)
+    end
+    FileUtils.chmod 0755, dest
+  end
 end
